ea1a2cb67ebffa7b9ab692f89982abaac4e01fda
4kari <mypasshidden@gmail.com>
2021-02-01 15:30:49 +0700

    Recovery Auth

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -82,7 +82,6 @@ class Auth extends CI_Controller
         );
 
         $cap = create_captcha($options);
-        var_dump($cap);
         $image = $cap['image'];
         // var_dump($cap['word']);
 
